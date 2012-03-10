51117fb7d5f07ac131d6d73ca3830c5bb11d45d2
Artem Shnayder <artem.shnayder@gmail.com>
2012-03-10 14:04:13 -0800

    resetting author

--- gony.py
+++ gony.py
@@ -5,6 +5,42 @@ import re
 import requests
 from BeautifulSoup import BeautifulSoup
 
+class Event:
+
+  def __init__(self, time, play, score):
+    self.time = time
+    self.play = play
+    self.score = score
+
+  def string(self):
+   return self.time, self.play, self.score
+
+class Player:
+  
+  def __init__(self, name):
+    self.name = name
+    self.fga = 0
+    self.fgm = 0
+
+  def missed(self):
+    self.fga = self.fga + 1
+
+  def made(self):
+    self.fga = self.fga + 1
+    self.fgm = self.fgm + 1
+
+  def __key(self):
+    return (self.name)
+
+  def __eq__(x, y):
+    return x.__key() == y.__key()
+
+  def __hash__(self):
+    return hash(self.__key())
+
+  def fg_line(self):
+    return 'name', self.name, ': ', self.fgm, ' of ', self.fga
+
 r = requests.get('http://scores.espn.go.com/nba/playbyplay?gameId=320226031&period=0')
 
 doc = r.text
@@ -12,6 +48,9 @@ soup = BeautifulSoup(doc)
 
 play_rows = soup.findAll('tr', { "class" : ['even','odd'] })
 
+events = list()
+
+# parse html
 for tag in play_rows:
   time = tag.find(style='text-align:center;').string
   play = tag.find(style='text-align:left;')
@@ -25,5 +64,31 @@ for tag in play_rows:
     score = score.string
   if play is None and score is None:
     play = tag.td.nextSibling.next.string
-  print time, play, score
+  event = Event(time, play, score)
+  events.append(event)
+
+players = dict()
+
+for event in events:
+  m = re.match('(.*?) makes (.*)$', event.play)
+  if m is not None and m.group(2)[0:10] != 'free throw':
+    name = m.group(1)
+    if name not in players:
+      players[name] = Player(name)
+    player = players[name]
+    player.made()
+    continue
   
+  m = re.match('(.*?) misses (.*)$', event.play)
+  if m is not None:
+    print m.group(2)[0:10]
+  if m is not None and m.group(2)[0:10] != 'free throw':
+    name = m.group(1)
+    if name not in players:
+      players[name] = Player(name)
+    player = players[name]
+    player.missed()
+    continue
+ 
+for player in players.values():
+  print player.fg_line()
